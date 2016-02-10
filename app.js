a583ae48a2c75b51e66e9cb63e9cf39502492839
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2016-02-10 10:49:36 +0200

    Singleton pattern

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+(function(){
+  "use strict";
 
+  var Moosipurk = function() {
+
+    // SINGLETON PATTERN
+    if(Moosipurk.instance) {
+      return Moosipurk.instance;
+    }
+
+    Moosipurk.instance = this; // this viitab moosipurgile
+
+    console.log('moosipurgi sees');
+
+    };
+
+    window.onload = function() {
+      var app = new Moosipurk();
+
+    };
+
+
+
+}) ();
