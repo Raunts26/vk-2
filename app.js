aa1489b99ab68dab8733560a430ace1605c85281
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2016-02-10 11:05:26 +0200

    protype jms

--- app.js
+++ app.js
@@ -10,9 +10,31 @@
 
     Moosipurk.instance = this; // this viitab moosipurgile
 
-    console.log('moosipurgi sees');
+    console.log(this);
+    //console.log('moosipurgi sees');
+    this.init(); // Panen rakenduse tööle
+    };
+
+    //Kõik moosipurgi fn tulevad siia sisse
+    Moosipurk.prototype = {
+      init: function() {
+        console.log('Rakendus OK');
+        //Siia tuleb esialgne loogika
+        //Hakka kuulama hiireklõpse
+        this.bindMouseEvents();
+      },
+      bindMouseEvents: function() {
+        document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+      },
+      addNewClick: function(event) {
+        console.log(event);
+
+      }
+
+
 
     };
+
 
     window.onload = function() {
       var app = new Moosipurk();
